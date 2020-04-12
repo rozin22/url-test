70312a69b49a18e3df375fd82f0f5eaeec36fbc1
Muhammad Rozi <wb-muhamadro518504@antfin.com>
2020-04-12 11:37:19 +0700

    Initial dist subtree commit

---
+++ precache-manifest.e1d6aabbe9e06beb807a8e63d7b5dce9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf17b8d68cad995248ec",
+    "url": "/url-test/css/app.680b1b4d.css"
+  },
+  {
+    "revision": "efd5c3e82a33244c99fa",
+    "url": "/url-test/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "1986850794add480da1b67c17e498e2a",
+    "url": "/url-test/index.html"
+  },
+  {
+    "revision": "4db27697015c338c51cd",
+    "url": "/url-test/js/about.e5bf1c24.js"
+  },
+  {
+    "revision": "bf17b8d68cad995248ec",
+    "url": "/url-test/js/app.e935391d.js"
+  },
+  {
+    "revision": "efd5c3e82a33244c99fa",
+    "url": "/url-test/js/chunk-vendors.f275b048.js"
+  },
+  {
+    "revision": "e6df5bf4022324915f1c0fdc69cc3301",
+    "url": "/url-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/url-test/robots.txt"
+  }
+]);
