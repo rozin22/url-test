ad5dc68507c9e8cf6d9b79e3bd44ba9ab828efd4
Muhammad Rozi <wb-muhamadro518504@antfin.com>
2020-04-12 11:31:54 +0700

    add input url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="md-layout-item">
+      <md-content class="md-elevation-10">
+        <div class="input-field">
+          <md-field>
+            <label>URL Link</label>
+            <md-input
+              @keyup.enter.native="goLocation"
+              ref="url"
+              v-model="URL"
+            ></md-input>
+          </md-field>
+        </div>
+        <div class="goto">
+          <md-button class="md-raised md-primary" @click="goLocation"
+            >Go to</md-button
+          >
+        </div>
+      </md-content>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+  data: () => {
+    return {
+      URL: ""
+    };
+  },
+
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+
+  methods: {
+    goLocation() {
+      location.href = "http://" + this.URL;
+      this.URL = "";
+    }
   }
 };
 </script>
+<style>
+.input-field {
+  margin: 1rem;
+}
+
+.goto {
+  padding: 0.2rem 0.2rem;
+  display: flex;
+}
+
+.md-layout-item {
+  margin: 2rem;
+}
+</style>
